ba80036edee753019c1e67fc9a00c8277bbbf879
Sebi Iordache <sebi.iordache@icloud.com>
2025-04-09 17:00:24 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -4,9 +4,9 @@
 #include <string>
 using namespace std;
 
-#define SCALE 10		// factor de scalare pentru marirea QR-ului
-#define NUM_THREADS 4	// nr de thread-uri pentru OpenMP
-#define BLOCK_SIZE 32	// dimensiunea blocului CUDA
+#define SCALE 10		    // factor de scalare pentru marirea QR-ului
+#define NUM_THREADS 4	  // nr de thread-uri pentru OpenMP
+#define BLOCK_SIZE 32	  // dimensiunea blocului CUDA
 
 // functii pentru generarea codului QR
 void generate_qr_secvential(const string& text);
